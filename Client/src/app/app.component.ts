c132519bbba1aad7567967322dbcedf601ff727a
ClementBoutet <clement.boutet@iut-dijon.u-bourgogne.fr>
2024-09-11 16:02:08 +0200

    ajustement de la taille des pièces

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GridComponent } from './grid/grid.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -31,6 +32,9 @@ export class AppComponent implements AfterViewInit{
       document.body.style.color = "white";
       document.getElementById("navbar-container")!.style.setProperty("background", "grey", "important");
       document.getElementById("navbar-container")!.style.setProperty("color", "white", "important");
+      Array.from(document.getElementsByClassName("timer")).forEach(timer=>{
+        (timer as HTMLDivElement).style.border = "1px solid white";
+      });
       (document.getElementById("logo") as HTMLImageElement).src = "renard_dark.png";
       (<HTMLButtonElement>document.getElementById("state")!).textContent = "Interface claire";
       this.state = "dark";
@@ -39,6 +43,9 @@ export class AppComponent implements AfterViewInit{
       document.body.style.color = "black";
       document.getElementById("navbar-container")!.style.setProperty("background", "#faf9fd", "important");
       document.getElementById("navbar-container")!.style.setProperty("color", "black", "important");
+      Array.from(document.getElementsByClassName("timer")).forEach(timer=>{
+        (timer as HTMLDivElement).style.border = "1px solid black";
+      });
       (<HTMLButtonElement>document.getElementById("state")!).textContent = "Interface sombre";
       (document.getElementById("logo") as HTMLImageElement).src = "renard.png"
       this.state = "light"
