95c604b41e07fb615396e08e3688b748ea198da4
ClementBoutet <clement.boutet@iut-dijon.u-bourgogne.fr>
2024-10-07 18:27:34 +0200

    recher d'une websocket qui marche

--- Client/src/app/game-screen/game-screen.component.ts
+++ Client/src/app/game-screen/game-screen.component.ts
@@ -9,17 +9,4 @@ import { GridComponent } from '../grid/grid.component';
   styleUrl: './game-screen.component.css'
 })
 export class GameScreenComponent {
-
-  public constructor(){
-    alert("ok")
-    const ws = new WebSocket("wss://127.0.0.1:7000");
-
-    ws.onopen = ()=>{
-      console.log("connected");
-    }
-
-    ws.onerror = (event) => {
-      console.error("WebSocket error observed:", event);
-  };
-  }
 }
