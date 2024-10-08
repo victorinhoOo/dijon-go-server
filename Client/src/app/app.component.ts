22c989e7a21558787d46c35c360d484f973b453a
victorinhoOo <dubozvictor@gmail.com>
2024-10-08 12:42:00 +0200

    Création de la page d'accueil

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GridComponent } from './grid/grid.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { IndexComponent } from './index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GridComponent, NavbarComponent],
+  imports: [RouterOutlet, GridComponent, NavbarComponent, IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Client/src/app/index/index.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+  
+  // Méthode pour remplir le leaderboard avec des données fictives
+  populateLeaderboard(): void {
+    const leaderboard = document.querySelector('.leaderboard');
+    // temporaire
+    const fakeEntries = [
+      '1) Victor - 9 dan',
+      '2) Mathis - 7 dan',
+      '3) Clément -  2 dan',
+      '4) Louis - 1 kyu',
+      '5) Adam - 20 kyu'
+    ];
+    leaderboard!.innerHTML = '';
+
+    // Ajoute des entrées dans la div "leaderboard"
+    fakeEntries.forEach(entry => {
+      const p = document.createElement('p');
+      p.textContent = entry;
+      leaderboard!.appendChild(p);
+    });
+  }
+
+  ngOnInit() {
+    this.populateLeaderboard();
+  }
+}
