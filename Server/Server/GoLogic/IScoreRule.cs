a31201cc56d04930386ca27fc7836bf8750207b6
0KeTeR0 <louis.demeocq@gmail.com>
2024-10-10 16:20:48 +0200

    Ajout de l'interface IScoreRule pour le principe ouvert/fermer

---
+++ Server/Server/GoLogic/IScoreRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoLogic
+{
+    public interface IScoreRule
+    {
+        public int CalculateScoreBlack(int blackTerritory);
+
+        public int CalculateScoreWhite(int whiteTerritory);
+    }
+}
