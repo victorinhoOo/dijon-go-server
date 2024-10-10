c04bf31c064df1dbb08f57a0cad4b9a54c44523d
MattisGaloppin <Mattis_Galoppin@etu.u-bourgogne.fr>
2024-10-10 14:29:30 +0200

    ajout de la route index

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -6,5 +6,6 @@ import { IndexComponent } from './index/index.component';
 export const routes: Routes = [
     {path:"", component: IndexComponent},
     {path:"game", component: GameScreenComponent},
-    {path:"grid", component: GridComponent}
+    {path:"grid", component: GridComponent},
+    {path: "index", component: IndexComponent}
 ];
