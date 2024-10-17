5ad8b40fb02b4ae08ef230d0c69a70e67f193472
Victor Duboz <80703824+victorinhoOo@users.noreply.github.com>
2024-10-17 10:35:59 +0200

    Modification des dao pour gérer la suppression de partie et la récupération des utilisateurs

---
+++ Server/Server/WebSocket/Model/DAO/IUserDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocket.Model.DAO
+{
+    /// <summary>
+    /// Définit un standard pour les opérations liées aux utilisateurs dans la bdd
+    /// </summary>
+    public interface IUserDAO
+    {
+        /// <summary>
+        /// Récupère le nom d'utilisateur associé à un token
+        /// </summary>
+        /// <param name="token">Le token de l'utilisateur</param>
+        /// <returns>Le nom d'utilisateur associé au token</returns>
+        public string GetUsernameByToken(string token);
+    }
+}
---
+++ Server/Server/WebSocket/Model/DAO/UserDAO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocket.Model.DAO
+{
+    public class UserDAO
+    {
+
+        private IDatabase database;
+
+        public UserDAO()
+        {
+            this.database = new MySqlDatabase("Server=localhost;Database=dgs;User=root;Password=''");
+
+        }
+        public string GetUsernameByToken(string token)
+        {
+            string usernameResult = null;
+            database.Connect();
+
+            try
+            {
+                // on récupére l'utilisateur associé au token
+                string query = @"
+                SELECT u.idUser, u.username
+                FROM user u
+                INNER JOIN tokenuser t ON u.idToken = t.idToken
+                WHERE t.token = @token";
+
+                var parameters = new Dictionary<string, object>
+                {
+                    { "@token", token }
+                };
+
+                var result = database.ExecuteQuery(query, parameters);
+
+                if (result.Rows.Count > 0)
+                {
+                    usernameResult = result.Rows[0]["username"].ToString();
+                }
+            }
+            finally
+            {
+                database.Disconnect();
+            }
+            return usernameResult;
+        }
+    }
+}
