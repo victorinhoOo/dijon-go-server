2dab2c634ee47b74ab0dd2698bb6d3f1218d72e8
MattisGaloppin <Mattis_Galoppin@etu.u-bourgogne.fr>
2024-11-15 15:51:48 +0100

    fix elo dans les cookies

--- Client/src/app/index/index.component.ts
+++ Client/src/app/index/index.component.ts
@@ -24,9 +24,17 @@ export class IndexComponent implements OnInit {
   private userPseudo: string;
   private avatar: string;
   private userRank: string;
+  private elo: number;
   private gameDAO: GameDAO;
   private userDAO: UserDAO;
 
+    /**
+   * Getter pour l'affichage de l'elo
+   */
+  public get Elo(): number
+  {
+    return this.elo;
+  }
   /**
  * Getter pour le lien d'affichage de l'avatar
  */
@@ -58,6 +66,7 @@ export class IndexComponent implements OnInit {
     this.token = '';
     this.userPseudo = '';
     this.gameDAO = new GameDAO(httpClient);
+    this.elo = this.userCookieService.getUser()!.Elo;
     this.userRank = this.userCookieService.getUser()!.Rank;
     this.userDAO = new UserDAO(httpClient);
   }
@@ -77,6 +86,7 @@ export class IndexComponent implements OnInit {
     this.userCookieService.getUserObservable().subscribe((user: User | null) => {
       if (user) {
         this.userPseudo = user.Username;
+        this.elo = user.Elo;
         this.userRank = user.Rank;
         this.avatar = `https://localhost:7065/profile-pics/${this.userPseudo}`;
       }
