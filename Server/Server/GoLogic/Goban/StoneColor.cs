8e1c259ee377b2df94879a15d520fc9d3d3383ef
Louis Deméocq <louis.demeocq@gmail.com>
2024-11-28 19:13:32 +0100

    [StoneColor] Déplacer StoneColor dans le dossier Goban

---
+++ Server/Server/GoLogic/Goban/StoneColor.cs
@@ -0,0 +1,12 @@
+﻿namespace GoLogic.Goban
+{
+    /// <summary>
+    /// Enumération des différents états possibles des pierres
+    /// </summary>
+    public enum StoneColor // TODO: Renommer IshiStatus ou IntersectionStatus ou PointState
+    {
+        Black,
+        White,
+        Empty
+    }
+}
