ecfe50fe2ba488ea4ae1a4619748cf0d02cbdd38
Victor Duboz <80703824+victorinhoOo@users.noreply.github.com>
2024-12-03 15:41:27 +0100

    [GameController, GameDAO, GameManager] Ajout de la récupération des parties d'un joueur et des coups (GameState) de cette partie côté serveur

--- Server/Server/Server/Controllers/GameController.cs
+++ Server/Server/Server/Controllers/GameController.cs
@@ -31,7 +31,7 @@ namespace Server.Controllers
             IActionResult result = BadRequest(new { Message = "Impossible de récupérer les parties" });
             try
             {
-                List<GameInfoDTO> games = gameManager.GetAvailableGames();
+                List<AvailableGameInfoDTO> games = gameManager.GetAvailableGames();
                 result = Ok(new { Games = games });
                 logger.LogInformation("Liste des parties récupérée");
             }
@@ -42,5 +42,52 @@ namespace Server.Controllers
             }
             return result;
         }
+
+        /// <summary>
+        /// Récupère la liste des parties jouées par un joueur
+        /// </summary>
+        /// <param name="token">token utilisateur du joueur souhaitant récupérer la liste de ses parties</param>
+        /// <returns>Liste des parties</returns>
+        [HttpPost("Played-games")]
+        public IActionResult GetGamesPlayed(string token)
+        {
+            IActionResult result = BadRequest(new { Message = "Impossible de récupérer les parties" });
+            try
+            {
+                List<GameInfoDTO> games = gameManager.GetGamesByToken(token);
+                result = Ok(new { Games = games });
+                logger.LogInformation("Liste des parties récupérée");
+            }
+            catch (Exception ex)
+            {
+                result = BadRequest(new { Message = ex.Message });
+                logger.LogInformation("Erreur lors de la récupération de la liste des parties : " + ex.Message);
+            }
+            return result;
+        }
+
+        [HttpPost("Game-states")]
+        /// <summary>
+        /// Récupère la liste des "états" d'une partie correspondant à chaque coup
+        /// </summary>
+        /// <param name="idGame">L'id de la partie dont on souhaite récupérer la liste des coups</param>
+        /// <returns>Liste des coups / états de la partie</returns>
+        public IActionResult GetGameStatesByid(int idGame)
+        {
+            IActionResult result = BadRequest(new { Message = "Impossible de récupérer les coups de la partie" });
+            try
+            {
+                List<GameStateDTO> gameStates = gameManager.GetGameStatesByGameId(idGame);
+                result = Ok(new { GameStates = gameStates });
+                logger.LogInformation("Liste des parties récupérée");
+            }
+            catch (Exception ex)
+            {
+                result = BadRequest(new { Message = ex.Message });
+                logger.LogInformation("Erreur lors de la récupération des coups de la partie : " + ex.Message);
+            }
+            return result;
+        }
+
     }
 }
---
+++ Server/Server/Server/Model/DTO/GameStateDTO.cs
@@ -0,0 +1,44 @@
+﻿namespace Server.Model.DTO
+{
+    /// <summary>
+    /// Représente l'état d'une partie à un moment donnée pour le transfert au client
+    /// </summary>
+    public class GameStateDTO
+    {
+        private string board;
+
+        private int capturedBlack;
+
+        private int capturedWhite;
+
+        private int moveNumber;
+
+        /// <summary>
+        /// Renvoi l'état du board au moment de cet état de la partie
+        /// </summary>
+        public string Board { get => board; }
+
+        /// <summary>
+        /// Renvoi le nombre de pierres capturées par les noirs à ce moment de la partie
+        /// </summary>
+        public int CapturedBlack { get => capturedBlack; }
+
+        /// <summary>
+        /// Renvoie le nombre de pierres capturées par les blancs à ce moment de la partie
+        /// </summary>
+        public int CapturedWhite { get => capturedWhite; }
+
+        /// <summary>
+        /// Renvoi le numéro du coup de cet état de la partie
+        /// </summary>
+        public int MoveNumber { get => moveNumber; }
+
+        public GameStateDTO(string board, int capturedBlack, int capturedWhite, int moveNumber)
+        {
+            this.board = board;
+            this.capturedBlack = capturedBlack;
+            this.capturedWhite = capturedWhite;
+            this.moveNumber = moveNumber;
+        }
+    }
+}
