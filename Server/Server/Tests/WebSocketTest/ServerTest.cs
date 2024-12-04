0223641289453e1fb38027b54059254352761271
Victor Duboz <80703824+victorinhoOo@users.noreply.github.com>
2024-12-04 16:05:05 +0100

    [Gametest.cs] [ServerTest.cs] Correction des tests unitaires

--- Server/Server/Tests/WebSocketTest/ServerTest.cs
+++ Server/Server/Tests/WebSocketTest/ServerTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WebSocket.Model;
 using WebSocket;
+using DotNetEnv;
 
 
 namespace Tests.WebSockets
@@ -15,7 +16,10 @@ namespace Tests.WebSockets
         public void Test_MessageIsHandshakeRequest()
         {
             // Arrange
-            WebSocket.Server server = new WebSocket.Server();
+            DotNetEnv.Env.Load();
+            string serverIp = Env.GetString("SERVER_IP");
+            int serverPort = Convert.ToInt32(Env.GetString("SERVER_PORT"));
+            WebSocket.Server server = new WebSocket.Server(serverIp, serverPort);
             string handshakeMessage = "GET /chat HTTP/1.1\r\nHost: server.example.com\r\nUpgrade: websocket\r\n";
             string normalMessage = "Hello world";
 
