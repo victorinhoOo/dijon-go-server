bbbcfb7f58acd1e86829d9e19a2a5e7d358c7d87
Louis Deméocq <louis.demeocq@gmail.com>
2024-12-05 11:46:51 +0100

    [BasicTimer], [ISystemTimer] Ajout de la documentation manquante

--- Server/Server/GoLogic/Timer/ISystemTimer.cs
+++ Server/Server/GoLogic/Timer/ISystemTimer.cs
@@ -1,10 +1,33 @@
 ﻿namespace GoLogic.Timer;
 
+/// <summary>
+/// Interfaces pour les différents types de Timer
+/// </summary>
 public interface ISystemTimer
 {
+    /// <summary>
+    /// Récupère le temps total initial
+    /// </summary>
     TimeSpan TotalTime { get; }
+
+    /// <summary>
+    /// Lance l'écoulement du timer
+    /// </summary>
     void Start();
+
+    /// <summary>
+    /// Met en pause l'écoulement du timer
+    /// </summary>
     void Pause();
+
+    /// <summary>
+    /// Relance le décompte du timer
+    /// </summary>
     void Resume();
+
+    /// <summary>
+    /// Renvoie s'il reste du temps au timer
+    /// </summary>
+    /// <returns>True s'il reste du temps False sinon</returns>
     bool HasTimeRemaining();
 }
