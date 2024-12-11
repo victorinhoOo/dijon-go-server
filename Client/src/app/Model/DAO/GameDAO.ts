203e0dc6471cd7df8fc7ab8f1840c4e7080cb65a
ClementBoutet <clement.boutet@iut-dijon.u-bourgogne.fr>
2024-12-11 14:34:01 +0100

    [index.component.ts, GameDAO.ts] Possibilité de cliquer sur un bouton afin de revoir directement sa dernière partie jouée

--- Client/src/app/Model/DAO/GameDAO.ts
+++ Client/src/app/Model/DAO/GameDAO.ts
@@ -4,6 +4,11 @@ import { AvailableGameInfoDTO } from '../DTO/AvailableGameInfoDTO';
 import { environment } from '../../environment';
 import { GameInfoDTO } from '../DTO/GameInfoDTO';
 import { GameStateDTO } from '../DTO/GameStateDTO';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 /**
  * Gère les requêtes HTTP vers l'API concernant les parties de jeu
@@ -34,10 +39,12 @@ export class GameDAO {
    * @returns Un Observable qui émet la liste des parties jouées
    */
   public GetGamesPlayed(token: string): Observable<GameInfoDTO[]> {
-    const params = new HttpParams().set('token', token); 
+    const params = new HttpParams().set('token', token);
     return this.http
       .post<{ games: GameInfoDTO[] }>(
-        `${environment.apiUrl}/Games/Played-games`,null, { params }
+        `${environment.apiUrl}/Games/Played-games`,
+        null,
+        { params }
       )
       .pipe(map((response) => response.games));
   }
@@ -47,12 +54,12 @@ export class GameDAO {
    * @param id identifiant de la partie
    * @returns les informations de la partie
    */
-  public GetGameById(id:number): Observable<GameInfoDTO> {
-    const params = new HttpParams().set('id', id); 
+  public GetGameById(id: number): Observable<GameInfoDTO> {
+    const params = new HttpParams().set('id', id);
     return this.http
-      .post<{ game: GameInfoDTO }>(
-        `${environment.apiUrl}/Games/Game`,null, { params }
-      )
+      .post<{ game: GameInfoDTO }>(`${environment.apiUrl}/Games/Game`, null, {
+        params,
+      })
       .pipe(map((response) => response.game));
   }
 
@@ -61,14 +68,23 @@ export class GameDAO {
    * @param id identifiant de la partie
    * @returns les états de la partie
    */
-  public GetGameStatesById(id:number): Observable<GameStateDTO[]> {
-    const params = new HttpParams().set('id', id); 
+  public GetGameStatesById(id: number): Observable<GameStateDTO[]> {
+    const params = new HttpParams().set('id', id);
     return this.http
       .post<{ states: GameStateDTO[] }>(
-        `${environment.apiUrl}/Games/Game-states`,null, { params }
+        `${environment.apiUrl}/Games/Game-states`,
+        null,
+        { params }
       )
       .pipe(map((response) => response.states));
   }
 
-
+  public GetLastGameId(token: string): Observable<number> {
+    const params = new HttpParams().set('token', token);
+    return this.http
+      .post<{ id: number }>(`${environment.apiUrl}/Games/Last-game-id`, null, {
+        params,
+      })
+      .pipe(map((response) => response.id));
+  }
 }
